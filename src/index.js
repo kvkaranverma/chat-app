c7db5be8762f4ea939e9ff3d3f062e43a3f2e403
Karan Verma <karanv598@gmail.com>
2020-06-20 11:25:58 +0530

    socket.io events (Sending and recieving events to/from server from/to client)

--- src/index.js
+++ src/index.js
@@ -12,8 +12,17 @@ const publicDirectory = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectory))
 
-io.on('connection', () => {
+let count = 0
+io.on('connection', (socket) => {
     console.log('New web socket connection.')
+
+    socket.emit('countUpdated', count)
+
+    socket.on('increment', () => {
+        count++
+        //socket.emit('countUpdated', count)
+        io.emit('countUpdated', count)
+    })
 })
 
 server.listen(port, () => {
